2984187ea6945364bb6a6e188577f805afe91977
v0o0v <v0o0v@daum.net>
2020-03-05 17:48:33 +0900

    add punch animation to block

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        DOTween.Init();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private void OnMouseDown()
-    {
-        Vector2 str = new Vector2(0.1f, 0.1f);
-        transform.DOPunchScale(str, 1f);
     }
 }
