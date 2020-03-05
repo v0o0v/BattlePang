42e6dcb17a8d26f7108ef614034a06c6adee9563
v0o0v <v0o0v@daum.net>
2020-03-05 18:02:05 +0900

    add gamne Manager Object and Script

--- Assets/Scripts/GameMgr.cs
+++ Assets/Scripts/GameMgr.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameMgr : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        DOTween.Init();
+        
     }
 
     // Update is called once per frame
