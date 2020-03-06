70ee2771c4f278c83dce85310c682cf45ee059d8
v0o0v <v0o0v@daum.net>
2020-03-06 17:05:27 +0900

    add method GetTile

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -7,7 +6,7 @@ public class Board : MonoBehaviour
     private Dictionary<string, GameObject> m_TileDictionary = new Dictionary<string, GameObject>();
     private GameObject m_tilePrefab;
     public int m_width = 16;
-    public int m_heught = 16;
+    public int m_height = 16;
 
     // Start is called before the first frame update
     void Start()
@@ -18,23 +17,21 @@ public class Board : MonoBehaviour
 
     private void CreateTiles()
     {
-        for (int y = 0; y < m_heught; y++)
+        for (int y = 0; y < m_height; y++)
         {
             for (int x = 0; x < m_width; x++)
             {
                 string key = x.ToString() + "," + y.ToString();
                 GameObject newTileGo = Instantiate(m_tilePrefab, this.transform, true);
-                newTileGo.transform.position = new Vector2(x,y);
+                newTileGo.transform.position = new Vector2(x, y);
                 m_TileDictionary[key] = newTileGo;
-
             }
         }
-
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetTile(int x, int y)
     {
+        string key = x.ToString() + "," + y.ToString();
+        return m_TileDictionary[key];
     }
 }
